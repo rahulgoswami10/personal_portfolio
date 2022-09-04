7f96e9b4f4b081c9871ec0a48f51b5b23e3fbd7c
Rahul Goswami <therahulgoswami99@gmail.com>
2022-09-04 18:41:18 +0530

    changed

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -67,7 +67,7 @@ const Testimonials = () => {
             return (
               <SwiperSlide key={index} className="testimonial">
               <div className="client__avatar" data-aos="zoom-in">
-                <img src={avatar} />
+                <img src={avatar} alt="my avatar"/>
               </div>  
               <h5 className="client__name">{name}</h5>
               <small className="client__review">{review}</small>
